fa5f749441a3471851e46f90ad2310ba7c607246
Vincent Cuvilliez <vcn.cvz@icloud.com>
2020-10-21 19:53:46 +0200

    Skills Check + Update
    Skills Check done
    Contenu intégré à 98%
    Animation done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let observer = new IntersectionObserver(function (observables){
     observables.forEach(function (observable){
-        if (observable.intersectionRatio > 0.5){
+        if (observable.intersectionRatio > 0.75){
             observable.target.classList.remove('not-visible')
             // observer.unobserve(observable.target)
         } else {
@@ -8,11 +8,11 @@ let observer = new IntersectionObserver(function (observables){
         }
     })
 }, {
-    threshold: [0.5]
+    threshold: [0.75]
 
 })
 
-let items = document.querySelectorAll('.text, .image')
+let items = document.querySelectorAll('.text, .image, .animation-top, .animation-top-left, .animation-top-right, .animation-bottom, .animation-bottom-left, .animation-bottom-right, .animation-left, .animation-right')
 items.forEach(function (item){
     item.classList.add('not-visible')
     observer.observe(item)
